b14daf2a6e2d8f1a2d7cf4cb8441346e3e92cbbb
root <root@debian12.top>
2024-06-19 15:06:25 +0700

    ganti

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -59,7 +59,7 @@ class KategoriController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Kategori $kategori)
+    public function update(Request $request, string $id)
     {
         //
         $kategori = Kategori::find($id);
@@ -84,7 +84,7 @@ class KategoriController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Kategori $kategori)
+    public function destroy(string $id)
     {
         //
         $kategori = Kategori::find($id);
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\KategoriController;
+// use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\Api\KategoriController;
+
+Route::apiResource('kategori', KategoriController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
